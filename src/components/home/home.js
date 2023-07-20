ec2c5c2d161338e27b78618db123a80e217d43bc
shrutibansal13 <shrutib@linkites.com>
2023-07-20 11:37:57 +0530

    functionalities added

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import ReactPaginate from 'react-paginate';
+import styles from './home.css';
+import {DebounceInput} from 'react-debounce-input';
 // import {toast} from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,57 +13,110 @@ import ReactPaginate from 'react-paginate';
 function Home() {
   const [page, setPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
-  const [search ,setSearch]  = useState('')
+  const [search ,setSearch]  = useState('');
   const [lengthh, setLength] = useState(0);
-  var totalpage =0;
-  var flag= false;
   const [users, setUsers] = useState([]);
+  var flag= false;
+
+
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems= users.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(users.length / itemsPerPage);
+
   const navigate = useNavigate();
-  if(lengthh>itemsPerPage){
-    totalpage= Math.ceil(lengthh/itemsPerPage);
-  }else{
-    totalpage=1
-  }
+  // if(lengthh>itemsPerPage){
+  //   totalpage= Math.ceil(lengthh/itemsPerPage);
+  // }else{
+  //   totalpage=1
+  // }
 
   // var itemOffset= (page-1)*itemsPerPage
 
   useEffect(() => {  
-    // searchuser(search)  
-    pageddata(search,page,itemsPerPage);
-    console.log(page,"page????????????????????");
-  }, [page,search])
+    getuser(search);
 
-  function getuser() {
-    try {
-      axios.get('http://localhost:8000/get').then((res) => {
-        // setUsers(res.data);
-        setLength(res.data.length);
-        console.log(res.data.length,">>>>>>>>>>>>>>>>>>>length");
-      })
+  
+    // searchuser(search)  
+    // pageddata(search,page,itemsPerPage);
+    // console.log(page,"page????????????????????");
+  }, [search])
+
+  function getuser(searching) {
+    if(searching===""){
+      try {
+        axios.get('http://localhost:8000/get').then((res) => {
+          setUsers(res.data);
+          setLength(res.data.length);
+          console.log(res.data.length,">>>>>>>>>>>>>>>>>>>length");
+        })
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }else{
+      try {
+        axios.get('http://localhost:8000/get').then((res) => {
+          var dat= res.data.filter((item)=>{
+            return searching.toLowerCase() === "" ? item : item.uname.toLowerCase().includes(searching);  
+         })
+         setUsers(dat)
+          setLength(res.data.length);
+          console.log(res.data.length,">>>>>>>>>>>>>>>>>>>length");
+        })
+    
+      }
+      catch (error) {
+        console.log(error);
+      }
     }
-    catch (error) {
-      console.log(error);
-    }
+   
+
+   
+
   }
  
-  function pageddata(search,page,limit) {
-    getuser()
-      try {
-      axios.get('http://localhost:8000/paging?' + new URLSearchParams({
-      search:search,
-       page:page,
-       limit:limit
-      })).then((res) => {
+  // function pageddata(search,page,limit) {
+  //   getuser()
+  //   //   try {
+  //   //   axios.get('http://localhost:8000/paging?' + new URLSearchParams({
+  //   //   search:search,
+  //   //    page:page,
+  //   //    limit:limit
+  //   //   })).then((res) => {
 
-        setUsers(res.data);
+  //   //     setUsers(res.data);
       
 
-      })
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+  //   //   })
+  //   // }
+  //   // catch (error) {
+  //   //   console.log(error);
+  //   // }
+  // }
+
+  // const handleSearch = (value)=>{
+  //   setSearch(value);
+  //   console.log(value);
+  //   currentItems= users.filter((item)=>{
+  //     return value.toLowerCase() === "" ? item : item.uname.toLowerCase().includes(value);  
+  //  })
+  //  setUsers(currentItems.slice(itemOffset,endOffset))
+  //   // latestdata= currentItems.slice(itemOffset, endOffset);
+  // //  console.log(result);
+  // //  setUsers(result)
+  // //  currentItems.push(result);
+  // // console.log(latestdata,"lateeeeeeeeeee");                  
+  //  console.log(users,"currrrrrrrrrrr");
+  // //  setUsers(result)   
+  // }
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % users.length;
+   
+    setItemOffset(newOffset);
+
+  };
 
 
   function update(id) {
@@ -85,7 +140,7 @@ function Home() {
         
         // setUsers(res.data);
         console.log(res);
-        getuser()
+        getuser('')
       })
     }
     catch (error) {
@@ -101,7 +156,9 @@ function Home() {
         <h1>Users</h1>
        
         <div className='col-md-6'>
-        <input name="search" type="text" placeholder='Search Users' value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
+        <DebounceInput
+          debounceTimeout={300}
+          onChange={(e)=>{setSearch(e.target.value)}} />
           <br></br><br></br>
           <div className='item-container'>
             <table className="table  table-striped table-bordered">
@@ -114,9 +171,7 @@ function Home() {
 
                 </tr>
               </thead>
-              {users && users.filter((item)=>{
-                    return search.toLowerCase() === "" ? item : item.uname.toLowerCase().includes(search);  
-                 }).map((user) => (
+              {currentItems && currentItems.map((user) => (
                   
                 <tbody>
                   <tr>
@@ -141,15 +196,40 @@ function Home() {
               ))}
 
             </table>
-           
-        {
+
+
+{users.length>5 ?
+            <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+
+      />
+      :<div></div>
+}           
+        {/* {
         <div className='row px-4 py-1 '>
          
           <ul className="pagination justify-content-center">
             <li className="page-item " style={{display: page === 1 ? 'none' : 'block'}}>
               <button className="page-link" onClick={() =>setPage(page-1)  }>&laquo;</button>
             </li>
-            {/* {console.log(totalpage,"totlllll")} */}
+      
             {
               [...Array(totalpage)].map((data, index) => (
                 <li key={index} className={`page-item ${page === index + 1 ? 'active' : ''} `}>
@@ -162,8 +242,9 @@ function Home() {
             </li>
           </ul>
         </div>
-}
+} */}
 
+        
           </div>
           <br></br>
         </div>
